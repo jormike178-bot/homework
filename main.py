af04bad5932ca2e2672477381a97503c7590143f
jormike178 <jormike178>
2025-10-04 16:20:16 +0500

    hz

---
+++ main.py
@@ -0,0 +1,16 @@
+#def product(a,b):
+    #"""Унижает числа
+    #>>> product(2,3)
+    #6
+    #>>> product(4,3)
+    #3
+    #"""
+    #return a+b
+
+
+import unittest
+
+
+class TestoName(unittest.TestCase):
+    def test_speed_up(self):
+        
